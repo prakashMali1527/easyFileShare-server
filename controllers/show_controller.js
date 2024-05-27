a866457868203dff0472cc0ac3660cdc3fb7fd1c
prakashMali1527 <prakash.dakshana@gmail.com>
2024-05-28 03:10:09 +0530

    Fix keep both controller change to single

--- controllers/show_controller.js
+++ controllers/show_controller.js
@@ -13,31 +13,6 @@ module.exports.downloadPage = async function(req,res){
 
         return res.render('download',{
             uuid: file.uuid,
-            fileName: file.filename,
-            fileSize: file.size,
-            downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
-        });
-
-    }catch(err){
-        return res.render('download', {error: 'something went wrong!!!'});
-    }
-    
-}
-const File = require('../models/file');
-require('dotenv').config();
-
-module.exports.downloadPage = async function(req,res){
-
-    try{
-        let file = await File.findOne({uuid: req.params.uuid});
-
-        if(!file){
-            console.log('No such file exist');
-            return res.render('download', {error: 'Link expired'});
-        }
-
-        return res.render('download',{
-            uuid: file.uuid,
             fileName: file.fileName,
             fileSize: file.size,
             downloadLink: `${process.env.APP_BASE_URL}/files/download/${file.uuid}`
