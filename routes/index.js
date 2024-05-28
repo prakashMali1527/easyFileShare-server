f0bd9582d83a234fc1e164efe1df84ed03c565fb
prakashMali1527 <prakash.dakshana@gmail.com>
2024-05-28 06:41:54 +0530

    home page response send for testing

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 const router = require('express').Router();
 
+router.get('/', function (req, res) {
+    return res.send('<h1>Welcome to easyFileSharing</h1>');
+})
 router.use('/api', require('./api/index'));
 router.use('/files', require('./show'));
 
